bf89915563d6ecd318338c7ee9a4fa760949d38f
LucaHaverty <lucahaverty@gmail.com>
2024-09-15 22:12:24 -0700

    Quaternion and comments

--- ray-marching-lib/quaternion/quaternion.h
+++ ray-marching-lib/quaternion/quaternion.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <math.h>
+#include "../vec3/vec3.h"
+
 typedef struct Quaternion
 {
     float x;
@@ -8,8 +11,73 @@ typedef struct Quaternion
     float w;
 } Quaternion;
 
+/** Created a new quaternion with the given components. */
 static inline Quaternion quat_create(float x, float y, float z, float w)
 {
-    Quaternion newVector = {x, y, z};
-    return newVector;
+    Quaternion newQuat = {x, y, z, w};
+    return newQuat;
+}
+
+/** Multiplies a quaternion by a vector, effectively rotating the vector by the angle represented in the quaternion.
+ *
+ * @param quaternion a quaternion representing an angle.
+ * @param vec3 a vector to be rotated by the input quaternion.
+ *
+ * @returns The resulting vector after the multiplication.
+ */
+static inline vec3 quat_multiply_vec3(Quaternion quaternion, Vec3 vec3)
+{
+    // Quaternion components
+    float qx = quaternion.x;
+    float qy = quaternion.y;
+    float qz = quaternion.z;
+    float qw = quaternion.w;
+
+    // Vector as quaternion with w = 0
+    Quaternion vec_quat = {vec3.x, vec3.y, vec3.z, 0};
+
+    // Quaternion multiplication (q * vec)
+    Quaternion res1;
+    res1.x = qw * vec_quat.x + qx * vec_quat.w + qy * vec_quat.z - qz * vec_quat.y;
+    res1.y = qw * vec_quat.y - qx * vec_quat.z + qy * vec_quat.w + qz * vec_quat.x;
+    res1.z = qw * vec_quat.z + qx * vec_quat.y - qy * vec_quat.x + qz * vec_quat.w;
+    res1.w = -qx * vec_quat.x - qy * vec_quat.y - qz * vec_quat.z;
+
+    // Inverse quaternion (conjugate since quaternions are normalized)
+    Quaternion inv_quat = {-qx, -qy, -qz, qw};
+
+    // Quaternion multiplication (res1 * q^-1)
+    Quaternion res2;
+    res2.x = res1.w * inv_quat.x + res1.x * inv_quat.w + res1.y * inv_quat.z - res1.z * inv_quat.y;
+    res2.y = res1.w * inv_quat.y - res1.x * inv_quat.z + res1.y * inv_quat.w + res1.z * inv_quat.x;
+    res2.z = res1.w * inv_quat.z + res1.x * inv_quat.y - res1.y * inv_quat.x + res1.z * inv_quat.w;
+
+    // Return resulting vector
+    Vec3 result = {res2.x, res2.y, res2.z};
+    return result;
+}
+
+/** Converts a euler representation of an angle to a quaternion.
+ *
+ * @param euler a euler angle in radians.
+ *
+ * @returns A quaternion equivalent to the euler angle.
+ */
+static inline Quaternion quat_from_euler(Vec3 euler)
+{
+    // Compute half-angles
+    float cy = cosf(euler.y * 0.5f); // Yaw
+    float sy = sinf(euler.y * 0.5f);
+    float cp = cosf(euler.x * 0.5f); // Pitch
+    float sp = sinf(euler.x * 0.5f);
+    float cr = cosf(euler.z * 0.5f); // Roll
+    float sr = sinf(euler.z * 0.5f);
+
+    Quaternion q;
+    q.w = cr * cp * cy + sr * sp * sy;
+    q.x = sr * cp * cy - cr * sp * sy;
+    q.y = cr * sp * cy + sr * cp * sy;
+    q.z = cr * cp * sy - sr * sp * cy;
+
+    return q;
 }
--- ray-marching-lib/vec3/vec3.h
+++ ray-marching-lib/vec3/vec3.h
@@ -7,12 +7,18 @@ typedef struct Vec3
     float z;
 } Vec3;
 
+/** Creates a new vector with the given components. */
 static inline Vec3 vec3_create(float x, float y, float z)
 {
     Vec3 newVector = {x, y, z};
     return newVector;
 }
 
+/** Adds two vectors, storing the result in vector 'a'.
+ *
+ * @param a the first vector that will store the result.
+ * @param b the second vector to add to 'a'.
+ */
 static inline void vec3_add(Vec3 *a, const Vec3 *b)
 {
     a->x += b->x;
@@ -20,6 +26,11 @@ static inline void vec3_add(Vec3 *a, const Vec3 *b)
     a->z += b->z;
 }
 
+/** Multiplies two vectors, storing the result in vector 'a'.
+ *
+ * @param a the first vector that will store the result.
+ * @param b the second vector to multiply 'a' by.
+ */
 static inline void vec3_multiply_scalar(Vec3 *a, float scalar)
 {
     a->x *= scalar;
@@ -27,9 +38,22 @@ static inline void vec3_multiply_scalar(Vec3 *a, float scalar)
     a->z *= scalar;
 }
 
+/** Multiples two vectors, returning the result as a new vector.
+ *
+ * @param a the first input vector.
+ * @param b the second input vector.
+ *
+ * @returns A new vector resulting from the multiplication.
+ */
 static inline Vec3 vec3_multiply_scalar_copy(const Vec3 *a, float scalar)
 {
     return vec3_create(a->x * scalar, a->y * scalar, a->z * scalar);
 }
 
+/** Takes a step from a coordinate point in a direction by a certain step size. Stores the result in vector 'a'.
+ *
+ * @param a the initial coordinate point.
+ * @param direction a normalized vector representing the direction to step.
+ * @param stepSize the distance to travel in the given direction.
+ */
 void vec3_step(Vec3 *a, const Vec3 *direction, float stepSize);
